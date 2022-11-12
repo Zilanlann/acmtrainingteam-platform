0763d145efde0d7e25e7475cb204d83facae5196
吴晟昊 <wushenghaop@qq.com>
2022-11-12 12:09:17 +0800

    11:12 12:09

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,24 @@
-import express from "express";
-const app = express();
+import express from "express"
+import config from "config"
+import { createConnection } from "mysql"
+
+const app = express()
+
+const dbConfig = config.get("dbConfig")
+const connection = createConnection(dbConfig)
+console.log(`The database ${dbConfig.database} is connected.`)
 
 app.get("/", (req, res) => {
-    res.send("Hello");
+    res.send("Hello")
+
+    connection.query("SELECT 1 + 1 AS solution", function(error, results, fields) {
+        if (error) throw error
+        console.log("The solution is: ", results[0].solution)
+    })
 })
 
-// process.env.PORT is not set now
-const port = process.env.PORT || 5000;
+const port = config.get("serverPort") || 5000
 
 app.listen(port, () => {
-    console.log(`Server is running on PORT ${port}`);
+    console.log(`Server is running on PORT ${port}.`)
 })

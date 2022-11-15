a8c08b193b0831afe5459286020d3ca12e573d8d
吴晟昊 <wushenghaop@qq.com>
2022-11-15 20:14:12 +0800

    api user test

--- backend/server.js
+++ backend/server.js
@@ -2,16 +2,20 @@ import express from 'express';
 import config from 'config';
 import { createConnection } from 'mysql';
 import util from 'util';
-
-const app = express();
+import bodyParser from 'body-parser';
+import user from './api/user.js'
 
 const dbConfig = config.get('dbConfig');
 const connection = createConnection(dbConfig);
 console.log(`The database ${dbConfig.database} is connected.`);
 connection.query = util.promisify(connection.query);
 
-const port = config.get('serverPort') || 5000;
+const app = express();
+app.use(bodyParser.json());
 
+app.use('/api/users', user);
+
+const port = config.get('serverPort') || 5000;
 app.listen(port, () => {
   console.log(`Server is running on PORT ${port}.`);
 });

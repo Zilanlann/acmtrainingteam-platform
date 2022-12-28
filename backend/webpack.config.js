2d2452380518b95419de749a73140007a636e2e4
吴晟昊 <wushenghaop@qq.com>
2022-12-28 22:21:09 +0800

    use webpack to package the backend to a file

---
+++ backend/webpack.config.js
@@ -0,0 +1,19 @@
+import path from "path";
+import { fileURLToPath } from "url";
+import TerserPlugin from "terser-webpack-plugin";
+const __filenameNew = fileURLToPath(import.meta.url);
+const __dirnameNew = path.dirname(__filenameNew);
+
+export default {
+  mode: "production",
+  entry: "./server.js",
+  target: "node",
+  output: {
+    path: path.resolve(__dirnameNew, "dist"),
+    filename: "bundle.js",
+  },
+  optimization: {
+    minimizer: [new TerserPlugin({ terserOptions: { mangle: false } })],
+    // mangle false else mysql blow ups with "PROTOCOL_INCORRECT_PACKET_SEQUENCE"
+  },
+};

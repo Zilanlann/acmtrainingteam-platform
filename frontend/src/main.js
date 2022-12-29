574dabc151d4bec9f67692be0481bf2496a53309
吴晟昊 <wushenghaop@qq.com>
2022-12-29 20:45:23 +0800

    frontend docker compose

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,8 +6,12 @@ import ElementPlus from "element-plus";
 import * as ElementPlusIconsVue from "@element-plus/icons-vue";
 import "element-plus/dist/index.css";
 import axios from "axios";
-axios.defaults.baseURL = "http://localhost:5000/";
-
+const reg = /^localhost+/;
+if (reg.test(window.location.host)) {
+  axios.defaults.baseURL = "http://localhost:5000/";
+} else {
+  axios.defaults.baseURL = `${window.location.protocol}//${window.location.host}:5000/`;
+}
 const app = createApp(App);
 app.use(ElementPlus).use(router).mount("#app");
 app.use(VueCookies);

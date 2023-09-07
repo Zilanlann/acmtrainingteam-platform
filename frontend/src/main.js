ad704f755df5998fdd29cf17aa55060210dc1b48
Shenghao Wu <wushenghaop@qq.com>
2023-09-07 10:38:07 +0800

    fix bug of frondend axios port

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -22,12 +22,10 @@ VMdPreview.use(vuepressTheme, {
   Hljs: hljs,
 });
 
-const reg = /^localhost+/;
-if (reg.test(window.location.host)) {
-  axios.defaults.baseURL = "http://localhost:5000/";
-} else {
-  axios.defaults.baseURL = `${window.location.protocol}//${window.location.host}:5000/`;
-}
+const postIndex = window.location.host.indexOf(":");
+const hostOutOfPost = window.location.host.substring(0, postIndex);
+axios.defaults.baseURL = `${window.location.protocol}//${hostOutOfPost}:5000/`;
+
 const app = createApp(App);
 app.use(ElementPlus).use(router).mount("#app");
 app.use(VueCookies);
